6ee15613811ee0e0fa28d0875dac59539989fc02
aspedrosa <aspedrosa@ua.pt>
2022-07-25 18:24:26 +0100

    remove some unnecessary comments on zshrc

--- shells/zsh/zshrc.zsh
+++ shells/zsh/zshrc.zsh
@@ -142,27 +142,10 @@ SAVEHIST=5000
 
 # export MANPATH="/usr/local/man:$MANPATH"
 
-# You may need to manually set your language environment
-# export LANG=en_US.UTF-8
-
-# Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='mvim'
-# fi
 
 # Compilation flags
 # export ARCHFLAGS="-arch x86_64"
 
-# Set personal aliases, overriding those provided by oh-my-zsh libs,
-# plugins, and themes. Aliases can be placed here, though oh-my-zsh
-# users are encouraged to define aliases within the ZSH_CUSTOM folder.
-# For a full list of active aliases, run `alias`.
-#
-# Example aliases
-# alias zshconfig="mate ~/.zshrc"
-# alias ohmyzsh="mate ~/.oh-my-zsh"
 
 source $HOME/dotfiles/shells/aliases.sh
 source $HOME/dotfiles/shells/functions.sh
